425cdd3a05f7f4f8b8f4443ed9aaea76300250e1
ArtForYou <kakakunyukya@gmail.com>
2023-11-29 22:25:22 +0700

    Feat: Editing Layout Register, Editing Code To RegisterActivity

--- app/src/main/java/com/difa/difaapp/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/difa/difaapp/ui/auth/login/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.difa.difaapp.ui.auth.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.difa.difaapp.R
 import com.difa.difaapp.databinding.ActivityLoginBinding
+import com.difa.difaapp.ui.auth.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -11,5 +13,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tvSignUp.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
