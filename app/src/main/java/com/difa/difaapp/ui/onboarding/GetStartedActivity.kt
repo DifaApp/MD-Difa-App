477dd0f449bdec9957f8bcd3e7ba58ecd2fb0ca1
Difable app <150643236+DifaApp@users.noreply.github.com>
2023-12-04 17:31:31 +0700

    Feat: Update Get started Activity, add more string (#9)
    Co-authored-by: ArtForYou <kakakunyukya@gmail.com>

--- app/src/main/java/com/difa/difaapp/ui/onboarding/GetStartedActivity.kt
+++ app/src/main/java/com/difa/difaapp/ui/onboarding/GetStartedActivity.kt
@@ -3,10 +3,14 @@ package com.difa.difaapp.ui.onboarding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.difa.difaapp.R
+import com.difa.difaapp.databinding.ActivityGetStartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGetStartedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        binding = ActivityGetStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
