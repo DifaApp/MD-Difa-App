0623c52de847082cf3ddcc316c4b38d94ca1e343
ArtForYou <kakakunyukya@gmail.com>
2023-12-14 15:38:21 +0700

    Feat: Add AboutAppActivity, Add KebijakanActivity

---
+++ app/src/main/java/com/difa/difaapp/ui/home/aboutApp/AboutAppActivity.kt
@@ -0,0 +1,12 @@
+package com.difa.difaapp.ui.home.aboutApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.difa.difaapp.R
+
+class AboutAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_app)
+    }
+}
---
+++ app/src/main/java/com/difa/difaapp/ui/profile/kebijakan/KebijakanActivity.kt
@@ -0,0 +1,12 @@
+package com.difa.difaapp.ui.profile.kebijakan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.difa.difaapp.R
+
+class KebijakanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kebijakan)
+    }
+}
