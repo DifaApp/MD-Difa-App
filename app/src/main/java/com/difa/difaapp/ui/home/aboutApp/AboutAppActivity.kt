d1f195c47c497ee982a8edcafd263b78231762df
ArtForYou <kakakunyukya@gmail.com>
2023-12-16 00:48:13 +0700

    Minor: Update Home Page, Update Profile Page, Update About App Page

--- app/src/main/java/com/difa/difaapp/ui/home/aboutApp/AboutAppActivity.kt
+++ app/src/main/java/com/difa/difaapp/ui/home/aboutApp/AboutAppActivity.kt
@@ -3,6 +3,7 @@ package com.difa.difaapp.ui.home.aboutApp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.difa.difaapp.R
 import com.difa.difaapp.databinding.ActivityAboutAppBinding
 
@@ -21,22 +22,34 @@ class AboutAppActivity : AppCompatActivity() {
             finish()
         }
 
+        val selectedImage = ContextCompat.getDrawable(this, R.drawable.ic_arrow_up_24)
+        val notSelectedImage = ContextCompat.getDrawable(this, R.drawable.ic_arrow_down_24)
+
         binding.cardView1.setOnClickListener {
             isExpanded1 = !isExpanded1
             binding.hiddenContent1.visibility = if(isExpanded1) View.VISIBLE else View.GONE
             binding.separatorLine.visibility = if(!isExpanded1) View.VISIBLE else View.GONE
+            binding.expandBtn1.setImageDrawable(
+                if(isExpanded1) selectedImage else notSelectedImage
+            )
         }
 
         binding.cardView2.setOnClickListener {
             isExpanded2 = !isExpanded2
             binding.hiddenContent2.visibility = if(isExpanded2) View.VISIBLE else View.GONE
             binding.separatorLine2.visibility = if(!isExpanded2) View.VISIBLE else View.GONE
+            binding.expandBtn2.setImageDrawable(
+                if(isExpanded2) selectedImage else notSelectedImage
+            )
         }
 
         binding.cardView3.setOnClickListener {
             isExpanded3 = !isExpanded3
             binding.hiddenContent3.visibility = if(isExpanded3) View.VISIBLE else View.GONE
             binding.separatorLine3.visibility = if(!isExpanded3) View.VISIBLE else View.GONE
+            binding.expandBtn3.setImageDrawable(
+                if(isExpanded2) selectedImage else notSelectedImage
+            )
         }
     }
 }
