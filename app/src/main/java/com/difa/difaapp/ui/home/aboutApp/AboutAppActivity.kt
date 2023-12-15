e080415b1f76a7f2724eba9174ae90dadcd71f52
ArtForYou <kakakunyukya@gmail.com>
2023-12-15 09:50:59 +0700

    Minor: Update AboutAppActivity, Update views Kebijakan,setting, update_profile

--- app/src/main/java/com/difa/difaapp/ui/home/aboutApp/AboutAppActivity.kt
+++ app/src/main/java/com/difa/difaapp/ui/home/aboutApp/AboutAppActivity.kt
@@ -2,11 +2,16 @@ package com.difa.difaapp.ui.home.aboutApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.difa.difaapp.R
 import com.difa.difaapp.databinding.ActivityAboutAppBinding
 
 class AboutAppActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAboutAppBinding
+
+    private var isExpanded1 = false
+    private var isExpanded2 = false
+    private var isExpanded3 = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutAppBinding.inflate(layoutInflater)
@@ -14,6 +19,24 @@ class AboutAppActivity : AppCompatActivity() {
 
         binding.ivBackAboutApp.setOnClickListener {
             finish()
+        }
+
+        binding.cardView1.setOnClickListener {
+            isExpanded1 = !isExpanded1
+            binding.hiddenContent1.visibility = if(isExpanded1) View.VISIBLE else View.GONE
+            binding.separatorLine.visibility = if(!isExpanded1) View.VISIBLE else View.GONE
+        }
+
+        binding.cardView2.setOnClickListener {
+            isExpanded2 = !isExpanded2
+            binding.hiddenContent2.visibility = if(isExpanded2) View.VISIBLE else View.GONE
+            binding.separatorLine2.visibility = if(!isExpanded2) View.VISIBLE else View.GONE
+        }
+
+        binding.cardView3.setOnClickListener {
+            isExpanded3 = !isExpanded3
+            binding.hiddenContent3.visibility = if(isExpanded3) View.VISIBLE else View.GONE
+            binding.separatorLine3.visibility = if(!isExpanded3) View.VISIBLE else View.GONE
         }
     }
 }
