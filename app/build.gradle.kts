cd14e330bd0687b19640bb624e7d8429a31f1e1c
ArtForYou <kakakunyukya@gmail.com>
2023-12-16 20:49:37 +0700

    Minor: Update dependencies for firabase auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,8 @@ dependencies {
     // firabase
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     // testing
     testImplementation("junit:junit:4.13.2")
